e145c7b83b92ad7e64830273e453a0771294eea8
Xylitolwhc <whcshanghai@gmail.com>
2017-06-03 22:52:58 +0800

    UI 75% done

--- app/src/main/java/com/hackday/play/EditUmbrellaActivity.java
+++ app/src/main/java/com/hackday/play/EditUmbrellaActivity.java
@@ -6,9 +6,11 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -23,12 +25,14 @@ import java.util.Date;
 public class EditUmbrellaActivity extends Activity {
     private LocationInfor locationInfor;
     private EditText editText;
-    private TextView textView,time;
+    private TextView textView, time;
     private Button button;
     private RelativeLayout relativeLayout;
-    private ImageView imageView;
-    private Long selectedTime;
-    private Handler handler=new Handler(){
+    private ImageView imageView, addboy, addgirl, addsecret,clock,titleImg;
+    private LinearLayout backgroung;
+    private String selectedTime;
+    private int sex = 0;
+    private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
@@ -44,22 +48,79 @@ public class EditUmbrellaActivity extends Activity {
         button = (Button) findViewById(R.id.activity_add_Button);
         relativeLayout = (RelativeLayout) findViewById(R.id.activity_add_RelativeLayout);
         imageView = (ImageView) findViewById(R.id.activity_add_sex);
-        time=(TextView) findViewById(R.id.activity_add_time);
+        clock=(ImageView) findViewById(R.id.activity_add_time_ImageView);
+        time = (TextView) findViewById(R.id.activity_add_time);
+        addboy = (ImageView) findViewById(R.id.center_boy_img);
+        addgirl = (ImageView) findViewById(R.id.activity_add_addgirl);
+        addsecret = (ImageView) findViewById(R.id.activity_add_addsecret);
+        titleImg=(ImageView) findViewById(R.id.activity_add_Title_ImageView);
+        backgroung=(LinearLayout) findViewById(R.id.activity_add_background);
 
+        init();
+    }
+
+    private void init() {
         locationInfor = MyApplication.getLocationInfor();
         if (getIntent().getIntExtra("Mode", 0) == 0) {//编辑、发布模式
             textView.setVisibility(View.INVISIBLE);
             relativeLayout.setVisibility(View.INVISIBLE);
             button.setText("点击求帮助OvO");
+            View.OnClickListener listener = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    switch (v.getId()) {
+                        case R.id.center_boy_img: {
+                            addboy.setImageResource(R.drawable.addboyc);
+                            addgirl.setImageResource(R.drawable.addgirl);
+                            addsecret.setImageResource(R.drawable.addsecret);
+                            sex = 1;
+                            backgroung.setBackground(getResources().getDrawable(R.drawable.add_back));
+                            titleImg.setImageResource(R.drawable.add_banner_boy);
+                            break;
+                        }
+                        case R.id.activity_add_addgirl: {
+                            addboy.setImageResource(R.drawable.addboy);
+                            addgirl.setImageResource(R.drawable.addgirlc);
+                            addsecret.setImageResource(R.drawable.addsecret);
+                            sex = -1;
+                            backgroung.setBackground(getResources().getDrawable(R.drawable.add_back_girl));
+                            titleImg.setImageResource(R.drawable.add_banner_girl);
+                            break;
+                        }
+                        case R.id.activity_add_addsecret: {
+                            addboy.setImageResource(R.drawable.addboy);
+                            addgirl.setImageResource(R.drawable.addgirl);
+                            addsecret.setImageResource(R.drawable.addsecretc);
+                            sex = 0;
+                            backgroung.setBackground(getResources().getDrawable(R.drawable.add_back_secret));
+                            titleImg.setImageResource(R.drawable.add_banner_secret);
+                            break;
+                        }
+                    }
+                }
+            };
+            addsecret.setOnClickListener(listener);
+            addboy.setOnClickListener(listener);
+            addgirl.setOnClickListener(listener);
+            View.OnClickListener listener1=new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    selectedTime="";
+                }
+            };
+            time.setOnClickListener(listener1);
+            clock.setOnClickListener(listener1);
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    String text=editText.getText().toString();
-                    LocationInfor newLocationInfor=new LocationInfor();
+                    String text = editText.getText().toString();
+                    LocationInfor newLocationInfor = new LocationInfor();
                     newLocationInfor.setDetail(text);
                     newLocationInfor.setName(MyApplication.getName());
                     newLocationInfor.setTime(selectedTime.toString());
                     newLocationInfor.setId(MyApplication.getId());
+                    newLocationInfor.setSex(sex);
+                    newLocationInfor.setTime(selectedTime);
 
                 }
             });
@@ -68,7 +129,7 @@ public class EditUmbrellaActivity extends Activity {
             textView.setVisibility(View.INVISIBLE);
             time.setClickable(false);
             imageView.setVisibility(View.INVISIBLE);
-            if (locationInfor.getId() == MyApplication.getId()){
+            if (locationInfor.getId() == MyApplication.getId()) {
                 button.setText("删除");
                 button.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -76,8 +137,7 @@ public class EditUmbrellaActivity extends Activity {
 
                     }
                 });
-            }
-            else {
+            } else {
                 switch (locationInfor.getSex()) {
                     case 1:
                         button.setText("帮助他");
